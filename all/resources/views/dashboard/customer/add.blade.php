82e13277a3f5d9ab10da814b7740de4dd2400f29
lakib <lakibul@parallaxlogic.net>
2022-07-05 13:07:46 +0600

    view file modify

--- all/resources/views/dashboard/customer/add.blade.php
+++ all/resources/views/dashboard/customer/add.blade.php
@@ -2,42 +2,48 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8">
-                <br/>
-                <h4>Add New Customer</h4>
-                <hr/>
-                <br/>
-                <p class="text-center text-success">{{Session::get('message')}}</p>
-                <form action="{{route('customer.store')}}" method="post">
-                    @csrf
-
-                    <div class="form-group row">
-                        <label for="inputEmail3" class="col-md-3 col-form-label">Name</label>
-                        <div class="col-md-9">
-                            <input type="text" class="form-control" name="name" placeholder="Enter Your Name">
-                            @error('name')
-                            <span class="text-danger">Name is Required</span>
-                            @enderror
-                        </div>
-
-                    </div>
+            <div class="card">
+                <div class="col-md-12">
                     <br/>
-                    <div class="form-group row">
-                        <label for="inputPassword3" class="col-sm-3 col-form-label">Phone Number</label>
-                        <div class="col-sm-9">
-                            <input type="number" class="form-control" name="mobile" placeholder="Phone Number">
-                        </div>
-                    </div>
-                    <br/>
-                    <div class="form-group row justify-content-end">
-                        <div class="col-sm-9">
-                            <div>
-                                <button type="submit" class="btn btn-primary">Create New Customer</button>
+                    <h4>Add New Customer</h4>
+                    <hr/>
+                    <p class="text-center text-success">{{Session::get('message')}}</p>
+                    <div class="card-body">
+                        <form action="{{route('customer.store')}}" method="post">
+                            @csrf
+                            <div class="form-group row">
+                                <label for="name" class="col-md-3">Name</label>
+                                <div class="col-md-9">
+                                    <input type="text" class="form-control" name="name" id="name" placeholder="Enter Your Name">
+                                    @error('name')
+                                    <span class="text-danger">Name is Required</span>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
+                            <br/>
+                            <div class="form-group row">
+                                <label for="mobile" class="col-sm-3">Phone Number</label>
+                                <div class="col-sm-9">
+                                    <input type="number" class="form-control" name="mobile" id="mobile" placeholder="Phone Number">
+                                    @error('mobile')
+                                    <span class="text-danger">Mobile Number is Required</span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <br/>
+                            <div class="form-group row justify-content-end">
+                                <div class="col-sm-9">
+                                    <div>
+                                        <button type="submit" class="btn btn-primary">Create New Customer</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                </form>
+
+                </div>
             </div>
+
         </div>
     </div>
 
--- all/resources/views/dashboard/customer/index.blade.php
+++ all/resources/views/dashboard/customer/index.blade.php
@@ -1,105 +1,90 @@
 @extends('master.admin')
 @section('content')
-    <div class="container mt-5">
-        <div class="card">
-            <div class="row">
-                <div class="col-md-10">
-                    <form action="{{route('customer.profile')}}" method="post">
-                        @csrf
-                        <div>
-                            <select class="form-select min-w-150px me-3"  data-allow-clear="true" name="value">
-                                <option value="" disabled selected>Select by Profile</option>
-                                <option value="1" >With Profile</option>
-                                <option value="2" >Without Profile</option>
-                            </select>
-                        </div>
-                        <div class="form-group ">
-                            <button type="submit" class="btn btn-primary">Show</button>
-                        </div>
-                    </form>
-                </div>
-                <div class="text-right col-md-2">
-                    <a href="{{route('customer.add')}}" class="btn btn-warning my-3 text-right">Add Customer</a>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="card">
-            <div class="row">
-                <div class="col-12">
-                    {{--                <div class="col-md-3" style="margin-left: -13px;">--}}
-                    {{--                    <a href="javascript:void(0)" class="btn btn-warning my-3" onclick="openModal()">Add Customer</a>--}}
-                    {{--                    <a href="{{route('customer.add')}}" class="btn btn-warning my-3">Add Customer</a>--}}
-                    {{--                </div>--}}
-
-                    <input type="text" name="search" id="search" placeholder="Search here..." class="form-control">
-                    <div class="card">
-                        <p class="text-center text-success">{{Session::get('message')}}</p>
-                        <div class="table-data">
-                            <table id="datatable" class="table table-bordered" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
-                                <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Created By</th>
-                                    <th>Name</th>
-                                    <th>Mobile</th>
-                                    <th>Action</th>
-                                    <th>Profile</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($customers as $customer)
-                                    <tr>
-                                        <td scope="row">{{$loop->iteration}}</td>
-                                        @if($customer->admin_id == 1)
-                                            <td>Admin</td>
-                                        @else
-                                            <td>Merchant</td>
-                                        @endif
-                                        <td>{{$customer->name}}</td>
-                                        <td>{{$customer->mobile}}</td>
-                                        <td>
-                                            <a href="{{route('customer.edit', ['id' => $customer->id])}}" class="btn btn-success btn-sm icon">
-                                                <i class="fas fa-edit"> Edit</i>
-                                            </a>
-                                            <a href="{{route('customer.delete', ['id' => $customer->id])}}" class="btn btn-danger btn-sm icon">
-                                                <i class="fab fa-trash"> Delete</i>
-                                            </a>
-                                        </td>
-                                        <td>
-                                            <a href="{{route('profile.add', ['id' => $customer->id])}}" class="btn btn-info btn-sm icon">
-                                                <i class="fa fa-plus"> Add</i>
-                                            </a>
-                                            <a href="{{route('profile.manage')}}" class="btn btn-info btn-sm icon">
-                                                <i class="fa fa-edit"> Manage</i>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                            {!! $customers->links() !!}
-                        </div>
+    <div class="col-md-12">
+        <h2>Customer</h2>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <form action="{{route('customer.profile')}}" method="post">
+                    @csrf
+                    <div>
+                        <select class="form-select min-w-150px me-3" data-allow-clear="true" name="value">
+                            <option value="" disabled selected>Select by Profile</option>
+                            <option value="1">With Profile</option>
+                            <option value="2">Without Profile</option>
+                        </select>
                     </div>
+                    <div class="form-group ">
+                        <button type="submit" class="btn btn-primary">Show</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="text-right col-md-8">
+            <a href="{{route('customer.add')}}" class="btn btn-warning my-3">Add
+                Customer</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <input type="text" name="search" id="search" placeholder="Search here..." class="form-control">
+            <div class="card">
+                <p class="text-center text-success">{{Session::get('message')}}</p>
+                <div class="table-data">
+                    <table id="datatable" class="table table-bordered"
+                           style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Created By</th>
+                            <th>Name</th>
+                            <th>Mobile</th>
+                            <th>Action</th>
+                            <th>Profile</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($customers as $customer)
+                            <tr>
+                                <td scope="row">{{$loop->iteration}}</td>
+                                @if($customer->admin_id == 1)
+                                    <td>Admin</td>
+                                @else
+                                    <td>Merchant</td>
+                                @endif
+                                <td>{{$customer->name}}</td>
+                                <td>{{$customer->mobile}}</td>
+                                <td>
+                                    <a href="{{route('customer.edit', ['id' => $customer->id])}}"
+                                       class="btn btn-danger btn-sm ">
+                                        <i class="fa fa-edit"> Edit</i>
+                                    </a>
+                                    <a href="{{route('customer.delete', ['id' => $customer->id])}}"
+                                       class="btn btn-danger btn-sm">
+                                        <i class="fa fa-trash"> Remove</i>
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="{{route('profile.add', ['id' => $customer->id])}}"
+                                       class="btn btn-info btn-sm icon">
+                                        <i class="fa fa-plus"> Add</i>
+                                    </a>
+                                    <a href="{{route('profile.manage')}}" class="btn btn-info btn-sm icon">
+                                        <i class="fa fa-fill"> Manage</i>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                    {!! $customers->links() !!}
                 </div>
             </div>
         </div>
-
     </div>
 
-
-    <!-- Optional JavaScript -->
     @include('dashboard.customer.customer_modal');
     @include('dashboard.customer.customer_js');
-{{--    {!! Toastr::message() !!}--}}
-    <script>
-        var form = document.getElementById('form');
-        form.addEventListener('submit', showMessage);
 
-        function showMessage(event) {
-            alert("Your response has been recorded. (Not actually, this is just a demo!)");
-            event.preventDefault();
-        }
-    </script>
 @endsection
