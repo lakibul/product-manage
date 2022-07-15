b0b02e6a979b5eca8c50dc7c181b4576326dbd6f
lakibul <lakibul.cse@gmail.com>
2022-07-15 19:06:20 +0600

    start api work

---
+++ all/app/Http/Controllers/Api/DisableProductApiController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DisableProductApiController extends Controller
+{
+    public function disable()
+    {
+        
+    }
+}
