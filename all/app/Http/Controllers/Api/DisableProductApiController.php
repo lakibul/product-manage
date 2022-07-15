0c050151b77d0fbf78a05888eeaa846c70cf8dc4
lakibul <lakibul.cse@gmail.com>
2022-07-15 19:12:28 +0600

    dd

--- all/app/Http/Controllers/Api/DisableProductApiController.php
+++ all/app/Http/Controllers/Api/DisableProductApiController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\DisableProduct;
+use App\Models\Inventory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DisableProductApiController extends Controller
 {
     public function disable()
     {
-
+        $inventory_product = Inventory::find($id);
+        DisableProduct::create([
+            'merch_id' => Auth::guard('merchant')->user()->id,
+            'product_id' => $inventory_product->product_id,
+            'inventory_id' => $inventory_product->id,
+        ]);
+        $inventory_product->status = 0;
+        $inventory_product->save();
     }
 }
