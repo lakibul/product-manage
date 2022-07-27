29c631950a63f636c6cf39a33ac2862aba379523
lakibul <lakibul.cse@gmail.com>
2022-07-27 18:46:33 +0600

    done

--- all/app/Models/File.php
+++ all/app/Models/File.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class File extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'filenames',
+    ];
+
+    public function others(): MorphMany
+    {
+        return $this->morphMany(Other::class, 'origin');
+    }
 }
